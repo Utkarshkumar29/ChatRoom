4aebcf8f9832675716cec6bedd754b62dae2211d
Utkarsh <ukyp2014@gmail.com>
2024-09-22 20:55:09 +0530

    protect fixing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,22 +12,20 @@ const protect = asyncHandler(async (req, res, next) => {
     const token = req.headers.authorization && req.headers.authorization.startsWith('Bearer') 
         ? req.headers.authorization.split(' ')[1] 
         : null;
-
+        console.log(token)
     if (!token) {
         res.status(401).send("Not authorized, please log in");
         throw new Error("Not authorized");
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('Decoded Token:', decoded); // Log decoded token
-        req.user = await User.findById(decoded.id).select("-password");
-        console.log('User Found:', req.user); // Log user information
+        
+        console.log(token)
         next();
     } catch (error) {
         console.error(error); // Log error details
-        res.status(401).json({ message: "Not authorized, token failed" });
-        throw new Error("Not authorized, token failed");
+        res.status(401).json({ message: `Not authorized, toke:${token} failed`,token,decoded });
+        
     }
 });
 
