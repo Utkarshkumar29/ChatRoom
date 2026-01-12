5bdf79b9dd8df5159584495a6f690df572b0763e
utkarshkumar29 <ukyp2014@gmail.com>
2026-01-12 21:40:27 +0530

    hero section fix #3

--- frontend/src/pages/landingPage/components/HeroSection.jsx
+++ frontend/src/pages/landingPage/components/HeroSection.jsx
@@ -1,10 +1,10 @@
-import user from "../../../assets/images/photo-1494790108377-be9c29b29330.jpg"
+import user from "../../../assets/images/photo-1494790108377-be9c29b29330.jpg";
 
 const HeroSection = () => {
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
-      <div className=" relative max-w-[1500px] mx-auto px-4 md:px-6 lg:px-8 py-12 md:py-16 lg:py-20 ">
-        <div className="w-full grid lg:grid-cols-2 gap-8 md:gap-12 lg:gap-16 items-center">
+      <div className=" relative max-w-[1500px] h-full mx-auto px-4 md:px-6 lg:px-8 py-12 md:py-16 lg:py-20 ">
+        <div className="w-full h-full grid lg:grid-cols-2 gap-8 md:gap-12 lg:gap-16 items-center">
           <div className=" text-left flex gap-10 flex-col ">
             <h1 className=" lg:text-7xl font-bold text-[#2D3748] ">
               Transform Team Chaos Into Organized Conversations
@@ -28,31 +28,85 @@ hover:bg-[#FF6B47]/90 hover:shadow-2xl hover:shadow-[#FF6B47] transition-all dur
             </div>
 
             <div className=" flex gap-10 ">
-                <div className="  flex items-center gap-2 ">
-                    <span className="inline-block w-[8px] h-[8px] rounded-full bg-green-500 transition-colors "></span>
+              <div className="  flex items-center gap-2 ">
+                <span className="inline-block w-[8px] h-[8px] rounded-full bg-green-500 transition-colors "></span>
 
-                    <span className=" text-[#718096]  ">12,847 teams online now</span>
-                </div>
-                <div>
-                    <span className="  text-[#718096] ">SOC 2 Certified</span>
-                </div>
+                <span className=" text-[#718096]  ">
+                  12,847 teams online now
+                </span>
+              </div>
+              <div>
+                <span className="  text-[#718096] ">SOC 2 Certified</span>
+              </div>
             </div>
           </div>
 
-          <div className=" shadow-xl w-[670px] h-[400px] rounded-2xl px-[24px] py-[16px] ">
-            <div className=" bg-blue-100 w-full h-full rounded-xl ">
-                <div className=" w-full h-full p-[12px] flex gap-4 ">
-                    <div className=" bg-[#2D3748] max-w-[40px] max-h-[40px] w-full h-full rounded-md  text-white flex items-center justify-center text-2xl ">#</div>
-                    <div className=" flex flex-col h-min ">
-                        <span className=" text-[16px] font-semibold ">Product Launch 2026</span>
-                        <span className=" text-[12px] ">24 members active</span>
-                    </div>
+          <div className=" flex gap-6 flex-col shadow-xl w-[670px]  rounded-2xl px-[24px] py-[16px] ">
+            <div className=" bg-blue-100 w-full rounded-xl p-[12px] flex flex-col gap-6 ">
+              <div className=" w-full flex gap-4 items-center ">
+                <div className=" bg-[#2D3748] w-[50px] h-[50px] rounded-md  text-white flex items-center justify-center text-2xl ">
+                  #
                 </div>
-                <div>
-                    <img src={user} className=" w-[20px] h-[20px] rounded-full " />
+                <div className=" flex flex-col h-min ">
+                  <span className=" text-[16px] font-semibold ">
+                    Product Launch 2026
+                  </span>
+                  <span className=" text-[12px] ">24 members active</span>
                 </div>
-                <div></div>
-                <div></div>
+              </div>
+              <div className=" flex-col flex gap-6 ">
+                <div className=" p-[8px] rounded-lg bg-white flex gap-2 items-center ">
+                  <img src={user} className=" w-[40px] h-[40px] rounded-full " />
+                  <div className=" flex flex-col ">
+                    <span>Sarah Chen</span>
+                    <span>
+                      Design mockups are ready for review. Check the files
+                      section.
+                    </span>
+                  </div>
+                </div>
+                <div className=" p-[8px] rounded-lg bg-white flex gap-2 items-center ">
+                  <img src={user} className=" w-[40px] h-[40px] rounded-full " />
+                  <div className=" flex flex-col ">
+                    <span>Sarah Chen</span>
+                    <span>
+                      Design mockups are ready for review. Check the files
+                      section.
+                    </span>
+                  </div>
+                </div>
+                <div className=" p-[8px] rounded-lg bg-white flex gap-2 items-center ">
+                  <img src={user} className=" w-[40px] h-[40px] rounded-full " />
+                  <div className=" flex flex-col ">
+                    <span>Sarah Chen</span>
+                    <span>
+                      Design mockups are ready for review. Check the files
+                      section.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" flex items-center gap-4 ">
+              <div className="flex items-center h-full">
+                <img
+                  src={user}
+                  className="w-[40px] h-[40px] rounded-full border border-white"
+                />
+                <img
+                  src={user}
+                  className="w-[40px] h-[40px] rounded-full border border-white -ml-3"
+                />
+                <img
+                  src={user}
+                  className="w-[40px] h-[40px] rounded-full border border-white -ml-3"
+                />
+                <div className="-ml-3 w-[40px] h-[40px] rounded-full bg-gray-700 text-white flex items-center justify-center text-sm border border-white">
+                  21+
+                </div>
+              </div>
+
+              <span className=" font-semibold text-gray-500 ">24 members in this room</span>
             </div>
           </div>
         </div>
